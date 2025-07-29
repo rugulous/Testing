502fa71ccd0fd08721ec16ee0c8802bef3fab651
Luke Taylor <luke@circleleasing.co.uk>
2025-07-29 13:11:46 +0100

    Add project files.

---
+++ TestingAPI/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        public bool Login([FromForm]LoginDetails login)
+        {
+            return login.Username == "lauren";
+        }
+    }
+
+    public record LoginDetails(string Username, string Password);
+}
