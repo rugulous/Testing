80a971b1c05bf6eaaa43b83c2dc315132d24e0d9
Luke Taylor <luke@circleleasing.co.uk>
2025-07-29 13:15:40 +0100

    Add bugged calculator

---
+++ TestingAPI/Controllers/CalculatorController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet("add")]
+        public string Add(string a, string b)
+        {
+            return a + b;
+        }
+
+        [HttpGet("subtract")]
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        [HttpGet("multiply")]
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        [HttpGet("divide")]
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
